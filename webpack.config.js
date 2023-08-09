ff43d2a62db47bcc88413f8e6daaca6c29d0d07b
Tatyana Nemchina <23angorka07@gmail.com>
2023-08-09 09:36:17 +0300

    Добавляет хеширование

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
 const path = require("path");
 const CopyPlagin = require("copy-webpack-plugin");
+const HTMLPlagin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/main.js",
   output: {
-    filename: "bundle.js",
+    filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "build"),
     clean: true,
   },
   devtool: "source-map",
   plugins: [
+    new HTMLPlagin({
+      template: "public/index.html",
+    }),
     new CopyPlagin({
-      patterns: [{ from: "public" }],
+      patterns: [
+        {
+          from: "public",
+          globOptions: {
+            ignore: ["**/index.html"],
+          },
+        },
+      ],
     }),
   ],
   module: {
