66fe050eddb8a3afd038f590145ba8fa3acc625b
Татьяна <tatyana@MacBook-Pro-Tatana.local>
2023-08-15 16:38:11 +0300

    делает отображение данных в компотенте динамическим

--- src/view/events-list-item-view.js
+++ src/view/events-list-item-view.js
@@ -1,14 +1,15 @@
 import { createElement } from '../render.js';
 
-function createEventsListItemTemplate() {
+function createEventsListItemTemplate(point) {
+  const {type, destination, basePrice} = point;
   return (
     `<li class="trip-events__item">
               <div class="event">
                 <time class="event__date" datetime="2019-03-18">MAR 18</time>
                 <div class="event__type">
-                  <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+                  <img class="event__type-icon" width="42" height="42" src="img/icons/${type.title}.png" alt="Event type icon">
                 </div>
-                <h3 class="event__title">Taxi Amsterdam</h3>
+                <h3 class="event__title">${type.title} ${destination.name}</h3>
                 <div class="event__schedule">
                   <p class="event__time">
                     <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
@@ -18,7 +19,7 @@ function createEventsListItemTemplate() {
                   <p class="event__duration">30M</p>
                 </div>
                 <p class="event__price">
-                  &euro;&nbsp;<span class="event__price-value">20</span>
+                  &euro;&nbsp;<span class="event__price-value">${basePrice}</span>
                 </p>
                 <h4 class="visually-hidden">Offers:</h4>
                 <ul class="event__selected-offers">
@@ -43,8 +44,12 @@ function createEventsListItemTemplate() {
 }
 
 export default class EventsListItemView {
+  constructor ({point}) {
+    this.point = point;
+  }
+
   getTemplate() {
-    return createEventsListItemTemplate();
+    return createEventsListItemTemplate(this.point);
   }
 
   getElement() {
